d64e1d482d79bd5f8c41c6902fa18d27ca1ac353
Martin Mittermaier <mittermaier@users.noreply.github.com>
2017-11-24 14:29:25 +0100

    Update dTSiede_dw_Loewer.m

--- dTSiede_dw_Loewer.m
+++ dTSiede_dw_Loewer.m
@@ -46,3 +46,29 @@ hilf=a0+a1*var1xi+a2*lnp+a3*var1xi.*lnp+a4*var1xi.^2+a5*lnp.^2+a6*var1xi.^2.*lnp
 hilf_strich=a1+a3*lnp+2*a4*var1xi+2*a6*var1xi.*lnp+a7*lnp.^2+2*a8*var1xi.*lnp.^2+3*a9*var1xi.^2+3*a11*var1xi.^2.*lnp+3*a12*var1xi.^2.*lnp.^2+a13*lnp.^3+2*a14*var1xi.*lnp.^3+3*a15*var1xi.^2.*lnp.^3;
 aus=-hilf_strich/(hilf^2);
 
+%Copyright 2017 Martin Mittermaier
+%    This program is free software: you can redistribute it and/or modify
+%    it under the terms of the GNU General Public License as published by
+%    the Free Software Foundation, either version 3 of the License, or
+%    (at your option) any later version.
+%
+%    This program is distributed in the hope that it will be useful,
+%    but WITHOUT ANY WARRANTY; without even the implied warranty of
+%    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+%    GNU General Public License for more details.
+%
+%    You should have received a copy of the GNU General Public License
+%    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+%
+%    Dieses Programm ist Freie Software: Sie können es unter den Bedingungen
+%    der GNU General Public License, wie von der Free Software Foundation,
+%    Version 3 der Lizenz oder (nach Ihrer Wahl) jeder neueren
+%    veröffentlichten Version, weiterverbreiten und/oder modifizieren.
+%
+%    Dieses Programm wird in der Hoffnung, dass es nützlich sein wird, aber
+%    OHNE JEDE GEWÄHRLEISTUNG, bereitgestellt; sogar ohne die implizite
+%    Gewährleistung der MARKTFÄHIGKEIT oder EIGNUNG FÜR EINEN BESTIMMTEN ZWECK.
+%    Siehe die GNU General Public License für weitere Details.
+%
+%    Sie sollten eine Kopie der GNU General Public License zusammen mit diesem
+%    Programm erhalten haben. Wenn nicht, siehe <http://www.gnu.org/licenses/>.
