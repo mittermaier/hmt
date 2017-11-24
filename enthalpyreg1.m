0cad1afb38931937cb20428982173903d5b87cd7
Martin Mittermaier <mittermaier@users.noreply.github.com>
2017-11-24 14:32:07 +0100

    Update enthalpyreg1.m

--- enthalpyreg1.m
+++ enthalpyreg1.m
@@ -1,6 +1,6 @@
 function enthalpyreg1=enthalpyreg1(temp, press)
 % specific enthalpy in region 1
-% Spezifische Enthalpie f�r Wasser im fl�ssigen Zustand
+% Spezifische Enthalpie für Wasser im flüssigen Zustand
 % enthalpyreg1  in kJ/kg
 % temp          in K
 % press         in bar
@@ -13,3 +13,30 @@ Stoffparameter_H2O;
 tau=1386 ./ temp;
 pic=0.1 .* press ./ 16.53;
 enthalpyreg1 = 0.001 .* rgas_water .* temp .* tau .* gammataureg1(tau, pic);
+
+%Copyright 2017 Martin Mittermaier
+%    This program is free software: you can redistribute it and/or modify
+%    it under the terms of the GNU General Public License as published by
+%    the Free Software Foundation, either version 3 of the License, or
+%    (at your option) any later version.
+%
+%    This program is distributed in the hope that it will be useful,
+%    but WITHOUT ANY WARRANTY; without even the implied warranty of
+%    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+%    GNU General Public License for more details.
+%
+%    You should have received a copy of the GNU General Public License
+%    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+%
+%    Dieses Programm ist Freie Software: Sie können es unter den Bedingungen
+%    der GNU General Public License, wie von der Free Software Foundation,
+%    Version 3 der Lizenz oder (nach Ihrer Wahl) jeder neueren
+%    veröffentlichten Version, weiterverbreiten und/oder modifizieren.
+%
+%    Dieses Programm wird in der Hoffnung, dass es nützlich sein wird, aber
+%    OHNE JEDE GEWÄHRLEISTUNG, bereitgestellt; sogar ohne die implizite
+%    Gewährleistung der MARKTFÄHIGKEIT oder EIGNUNG FÜR EINEN BESTIMMTEN ZWECK.
+%    Siehe die GNU General Public License für weitere Details.
+%
+%    Sie sollten eine Kopie der GNU General Public License zusammen mit diesem
+%    Programm erhalten haben. Wenn nicht, siehe <http://www.gnu.org/licenses/>.
