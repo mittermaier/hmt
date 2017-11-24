34548857e998bd54632602f7306a2f331fbaab2a
Martin Mittermaier <mittermaier@users.noreply.github.com>
2017-11-24 14:30:52 +0100

    Update enthalpySatLiqTH2OLiBr_PK.m

--- enthalpySatLiqTH2OLiBr_PK.m
+++ enthalpySatLiqTH2OLiBr_PK.m
@@ -1,5 +1,4 @@
 function enthalpySatLiqTH2OLiBr_PK=enthalpySatLiqTH2OLiBr_PK(temp,konz)
-
 % This function calculates the specific enthalpy of H2O-LiBr-solution 
 % temp                 Temperature             in K
 %
@@ -47,4 +46,29 @@ enthalpySatLiqW = enthalpySatLiqTW(temp).*1000.*M_W;               % in J/mol
 c=(konz_mol.*M_LiBr + (1-konz_mol).*M_W);  
 enthalpySatLiqTH2OLiBr_PK = ((1-konz_mol).*enthalpySatLiqW + enthalpyCritWmol.*mult)./1000./c; % in kJ/kg;
 
-
+%Copyright 2017 Martin Mittermaier
+%    This program is free software: you can redistribute it and/or modify
+%    it under the terms of the GNU General Public License as published by
+%    the Free Software Foundation, either version 3 of the License, or
+%    (at your option) any later version.
+%
+%    This program is distributed in the hope that it will be useful,
+%    but WITHOUT ANY WARRANTY; without even the implied warranty of
+%    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+%    GNU General Public License for more details.
+%
+%    You should have received a copy of the GNU General Public License
+%    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+%
+%    Dieses Programm ist Freie Software: Sie können es unter den Bedingungen
+%    der GNU General Public License, wie von der Free Software Foundation,
+%    Version 3 der Lizenz oder (nach Ihrer Wahl) jeder neueren
+%    veröffentlichten Version, weiterverbreiten und/oder modifizieren.
+%
+%    Dieses Programm wird in der Hoffnung, dass es nützlich sein wird, aber
+%    OHNE JEDE GEWÄHRLEISTUNG, bereitgestellt; sogar ohne die implizite
+%    Gewährleistung der MARKTFÄHIGKEIT oder EIGNUNG FÜR EINEN BESTIMMTEN ZWECK.
+%    Siehe die GNU General Public License für weitere Details.
+%
+%    Sie sollten eine Kopie der GNU General Public License zusammen mit diesem
+%    Programm erhalten haben. Wenn nicht, siehe <http://www.gnu.org/licenses/>.
