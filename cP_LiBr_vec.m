d15456544abe6bacfcd01cc502178aa3aa00640e
Martin Mittermaier <mittermaier@users.noreply.github.com>
2017-11-24 14:29:01 +0100

    Update cP_LiBr_vec.m

--- cP_LiBr_vec.m
+++ cP_LiBr_vec.m
@@ -44,3 +44,30 @@ a14=-1.81967E-07;
 a15=-3.38265E-07;
 aus=a0+a1*x+a2*T+a3*x.*T+a4*x.^2+a5*T.^2+a6*x.^2.*T+a7*x.*T.^2+a8*x.^2.*T.^2+a9*x.^3+a10*T.^3+a11*x.^3.*T+a12*x.^3.*T.^2+a13*x.*T.^3+a14*x.^2.*T.^3+a15*x.^3.*T.^3;
 aus=aus*1000;
+
+%Copyright 2017 Martin Mittermaier
+%    This program is free software: you can redistribute it and/or modify
+%    it under the terms of the GNU General Public License as published by
+%    the Free Software Foundation, either version 3 of the License, or
+%    (at your option) any later version.
+%
+%    This program is distributed in the hope that it will be useful,
+%    but WITHOUT ANY WARRANTY; without even the implied warranty of
+%    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+%    GNU General Public License for more details.
+%
+%    You should have received a copy of the GNU General Public License
+%    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+%
+%    Dieses Programm ist Freie Software: Sie können es unter den Bedingungen
+%    der GNU General Public License, wie von der Free Software Foundation,
+%    Version 3 der Lizenz oder (nach Ihrer Wahl) jeder neueren
+%    veröffentlichten Version, weiterverbreiten und/oder modifizieren.
+%
+%    Dieses Programm wird in der Hoffnung, dass es nützlich sein wird, aber
+%    OHNE JEDE GEWÄHRLEISTUNG, bereitgestellt; sogar ohne die implizite
+%    Gewährleistung der MARKTFÄHIGKEIT oder EIGNUNG FÜR EINEN BESTIMMTEN ZWECK.
+%    Siehe die GNU General Public License für weitere Details.
+%
+%    Sie sollten eine Kopie der GNU General Public License zusammen mit diesem
+%    Programm erhalten haben. Wenn nicht, siehe <http://www.gnu.org/licenses/>.
