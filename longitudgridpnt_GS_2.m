7a73a4f09cd509950744df0b8e9e1ff53d8dbef7
Martin Mittermaier <mittermaier@users.noreply.github.com>
2017-11-24 14:42:07 +0100

    Update longitudgridpnt_GS_2.m

--- longitudgridpnt_GS_2.m
+++ longitudgridpnt_GS_2.m
@@ -1,5 +1,4 @@
 function aus=longitudgridpnt_GS_2(z_vektor,n,nu,g,u0)
-
 % z_vektor		vector containing transversal grid points           	in m
 %                                                				[ ... ]
 % n			index of sought longitudinal grid point
@@ -28,6 +27,31 @@ function aus=longitudgridpnt_GS_2(z_vektor,n,nu,g,u0)
 
 aus=fzero(@(x)Filmdicke_hilf(z_vektor(1,n),nu,u0,g,x),[10^-19 1]);
 
-
+%Copyright 2017 Martin Mittermaier
+%    This program is free software: you can redistribute it and/or modify
+%    it under the terms of the GNU General Public License as published by
+%    the Free Software Foundation, either version 3 of the License, or
+%    (at your option) any later version.
+%
+%    This program is distributed in the hope that it will be useful,
+%    but WITHOUT ANY WARRANTY; without even the implied warranty of
+%    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+%    GNU General Public License for more details.
+%
+%    You should have received a copy of the GNU General Public License
+%    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+%
+%    Dieses Programm ist Freie Software: Sie können es unter den Bedingungen
+%    der GNU General Public License, wie von der Free Software Foundation,
+%    Version 3 der Lizenz oder (nach Ihrer Wahl) jeder neueren
+%    veröffentlichten Version, weiterverbreiten und/oder modifizieren.
+%
+%    Dieses Programm wird in der Hoffnung, dass es nützlich sein wird, aber
+%    OHNE JEDE GEWÄHRLEISTUNG, bereitgestellt; sogar ohne die implizite
+%    Gewährleistung der MARKTFÄHIGKEIT oder EIGNUNG FÜR EINEN BESTIMMTEN ZWECK.
+%    Siehe die GNU General Public License für weitere Details.
+%
+%    Sie sollten eine Kopie der GNU General Public License zusammen mit diesem
+%    Programm erhalten haben. Wenn nicht, siehe <http://www.gnu.org/licenses/>.
 
 
